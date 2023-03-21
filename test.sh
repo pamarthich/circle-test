629fffc9acbe9af87961686f8af953aedfc5acd8
pamarthich <33571885+pamarthich@users.noreply.github.com>
2023-03-21 14:40:15 -0700

    load env variables

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-export strawhats="MONKEY D LUFFY"
-export beastpirates="KAIDO"
+# export strawhats="MONKEY D LUFFY"
+# export beastpirates="KAIDO"
+echo 'strawhats="MONKEY D LUFFY"' >> "$BASH_ENV"
+echo 'beastpirates="KAIDO"' >> "$BASH_ENV"
+source "$BASH_ENV"
