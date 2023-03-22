f69355400d8120ccc605fe1da179a765e7541dfe
pamarthich <33571885+pamarthich@users.noreply.github.com>
2023-03-22 11:41:31 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Strawhats="MONKEY D LUFFY"
 # export beastpirates="KAIDO"
-echo 'strawhats="$Strawhats"' >> "$BASH_ENV"
+echo 'strawhats=$Strawhats' >> "$BASH_ENV"
 echo 'beastpirates="KAIDO"' >> "$BASH_ENV"
 source "$BASH_ENV"
